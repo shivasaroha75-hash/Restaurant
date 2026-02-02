76fb35efac11ec814e0b14876b53a14091d5065f
Shiva Saroha <shivasaroha75@gmail.com>
2026-02-02 15:52:01 +0530

    url updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://restaurant-backend-2p5u.onrender.com";
 
 // ================= RESTAURANTS =================
 export function fetchRestaurants() {
